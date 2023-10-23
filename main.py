c0603abdafc5c2d0cdbdc5dcb138d86968182473
Felix Beaulieu <felix.beaulieu80@gmail.com>
2023-10-23 08:01:35 -0400

    Updated gitignore and reformatted main file.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
+
 def get_date_taken(path):
     """Extract date taken from image metadata."""
     try:
@@ -13,6 +14,7 @@ def get_date_taken(path):
         return None
     except:
         return None
+
 
 def sort_images(directory):
     """Sort images based on their taken date."""
@@ -24,30 +26,32 @@ def sort_images(directory):
                 if date:
                     year, month = date[:4], date[5:7]
                     month_name = {
-                        '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', 
+                        '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr',
                         '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug',
                         '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
                     }.get(month, '')
-                    
+
                     if not month_name:
                         continue
-                    
+
                     year_folder = os.path.join(directory, year)
                     month_folder = os.path.join(year_folder, month_name)
-                    
+
                     if not os.path.exists(year_folder):
                         os.mkdir(year_folder)
                     if not os.path.exists(month_folder):
                         os.mkdir(month_folder)
-                    
+
                     shutil.move(file_path, os.path.join(month_folder, file))
     messagebox.showinfo("Info", "Images sorted successfully!")
+
 
 def select_directory():
     """Open directory selection dialog and sort images in the selected directory."""
     directory = filedialog.askdirectory()
     if directory:
         sort_images(directory)
+
 
 # Create the GUI window
 app = tk.Tk()
